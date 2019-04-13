0f741e224bba97dda9144fa7759308fb0dbb194a
ryanmarcwegmeier <ryan-wegmeier-96@win.tu-berlin.de>
2019-04-13 17:37:15 +0200

    Updates

---
+++ precache-manifest.2a4912575b5ea0074ec73dcea8e69d1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testrepo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5557c4e23ba3596b74df",
+    "url": "/testrepo/static/js/runtime~main.33c8664a.js"
+  },
+  {
+    "revision": "d21275ca38f974746a89",
+    "url": "/testrepo/static/js/main.15680d4e.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/testrepo/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "d21275ca38f974746a89",
+    "url": "/testrepo/static/css/main.a1f05d57.chunk.css"
+  },
+  {
+    "revision": "ea08243337db42fd24e6fa7621b58103",
+    "url": "/testrepo/index.html"
+  }
+];
